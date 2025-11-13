4c900e73b7dcb08ccf7f842df5de0728676be6fb
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-11-13 17:26:32 +0100

    added two helper functions to get image and file data

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,34 @@
-export const toQueryString = ( params ) => {
-  if (!params || Object.keys(params).length === 0 ) {
-    return '';
+export const toQueryString = (params) => {
+  if (!params || Object.keys(params).length === 0) {
+    return "";
   }
   return `?${new URLSearchParams(params).toString()}`;
+};
+export const getImage = (e) => {
+  const files = Array.from(e.target.files);
+  const newImages = files
+    .filter((file) => file.type.startsWith("image/"))
+    .map((file) => ({
+      id: Date.now() + Math.random(),
+      file,
+      name: file.name,
+      size: file.size,
+      url: URL.createObjectURL(file),
+      isUploading: true,
+    }));
+  return newImages;
+};
+export const getFile = (e) => {
+  const files = Array.from(e.target.files);
+  const newFiles = files
+    .filter((file) => !file.type.startsWith("image/"))
+    .map((file) => ({
+      id: Date.now() + Math.random(),
+      file,
+      name: file.name,
+      size: file.size,
+      type: file.type,
+      isUploading: true,
+    }));
+  return newFiles;
 };
