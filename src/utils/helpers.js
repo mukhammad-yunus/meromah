83514576c32aa8bce4803ad40a469eb84a8bd044
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-11-14 02:09:15 +0100

    feat: new helper functions to getFileUrl & handleDownload

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -43,3 +43,41 @@ export const getInitials = (name) => {
       .toUpperCase()
       .slice(0, 2);
   };
+export const getFileUrl = (hash) => {
+  const API_BASE_URL = import.meta.env.DEV
+    ? "/api"
+    : import.meta.env.VITE_API_BASE_URL || "/api";
+  return `${API_BASE_URL}/files/${hash}`;
+};
+export const handleDownload = async (file, e, downloadElement) => {
+    e.preventDefault();
+    e.stopPropagation();
+    downloadElement.disabled = true;
+    try {
+      const url = getFileUrl(file.hash);
+
+      const response = await fetch(url, {
+        method: "GET",
+        credentials: "include", // only if your API needs cookies
+      });
+
+      if (!response.ok) throw new Error("Failed to download");
+
+      const blob = await response.blob();
+
+      const objectUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = objectUrl;
+      link.download = file.filename;
+
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+
+      window.URL.revokeObjectURL(objectUrl);
+      downloadElement.disabled = false;
+    } catch (error) {
+      downloadElement.disabled = false;
+      console.error("Failed to download file:", error);
+    }
+  };
