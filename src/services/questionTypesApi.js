9c0f5a610d2f063ca8d3bc17f9cce964ca5dc68e
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-11-23 06:56:23 +0100

    feat(question-types): add transformResponse to format API data and expose mapped types

--- src/services/questionTypesApi.js
+++ src/services/questionTypesApi.js
@@ -1,9 +1,27 @@
+import { data } from "react-router-dom";
 import { baseApi } from "./baseApi";
 
 const questionTypesApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getQuestionTypes: builder.query({
       query: () => "question-types",
+      transformResponse: (response) => {
+        const result = {
+          data: [],
+        };
+
+        response.data.forEach((item) => {
+          result[item.name] = {
+            id: item.id,
+            type: item.name,
+            label: item.description,
+          };
+          if (item.name === "mcq" || item.name === "code") {
+            result.data.push({ type: item.name, label: item.description });
+          }
+        });
+        return result;
+      },
     }),
   }),
   overrideExisting: true,
